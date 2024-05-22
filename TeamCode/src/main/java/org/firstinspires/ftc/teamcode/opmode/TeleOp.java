c23d11f60c5c99e13e2772a317cb68cf1bd179ff
Pedro Henrique Rosa Lacerda da Silva <108750566+pedrohenriquelacerda@users.noreply.github.com>
2024-05-22 18:33:12 -0300

    :recycle: refactor: LinearOpMode to OpMode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/TeleOp.java
@@ -1,24 +1,28 @@
 package org.firstinspires.ftc.teamcode.opmode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 import org.firstinspires.ftc.teamcode.hardware.RobotHardware;
 import org.firstinspires.ftc.teamcode.subsystems.Drivetrain;
 
 @com.qualcomm.robotcore.eventloop.opmode.TeleOp (name = "Teste")
-public class TeleOp extends LinearOpMode {
+public class TeleOp extends OpMode {
 
     @Override
-    public void runOpMode() throws InterruptedException {
+    public void init() {
         RobotHardware.setHardwareMap(hardwareMap);
         RobotHardware.init();
-        while (opModeIsActive()) {
-            loopRobot();
-        }
     }
 
-    public void loopRobot() {
+    @Override
+    public void loop() {
         Drivetrain.getInstance().arcadeDrive(-gamepad1.left_stick_y, gamepad1.right_stick_x);
+    }
+
+    @Override
+    public void stop() {
+        Drivetrain.getInstance().stop();
     }
 
 }

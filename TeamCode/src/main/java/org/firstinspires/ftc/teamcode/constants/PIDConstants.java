3c544c3b56bf216ba5d012066cf6b1e019d6d2a8
Davi Feitosa <davifeitosassa@gmail.com>
2024-05-25 16:08:23 -0300

    :fire: fix(pid): delete PID and PIDConstants files

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/constants/PIDConstants.java
+++
@@ -1,7 +0,0 @@
-package org.firstinspires.ftc.teamcode.constants;
-public class PIDConstants {
-    public static double kp =1.8; //proportional value for the PID
-    public static double ki = 0.0; //integral value for the PID
-    public static double kd = 0.031; //derivative value for the PID
-    public static double kf = 0.1;
-}

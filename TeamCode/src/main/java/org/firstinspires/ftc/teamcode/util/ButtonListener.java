e6df2e1eeb6e981c33d75e669690116441aca3c2
Pedro Henrique Rosa Lacerda da Silva <108750566+pedrohenriquelacerda@users.noreply.github.com>
2024-05-27 23:19:19 -0300

    :memo: Javadocs(ButtonListener): Add documentation for ButtonListener.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/ButtonListener.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/ButtonListener.java
@@ -4,6 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * ButtonListener is a utility class that listens for a button press and runs a Runnable if the button is pressed
+ * or a Runnable if the button is not pressed.
+ * <p>
+ * It can also listen for multiple buttons and do comparisons.
+ */
 public class ButtonListener {
 
     private final List<Boolean> buttonList;
@@ -16,10 +22,26 @@ public class ButtonListener {
         this.buttonList = new ArrayList<>();
     }
 
+    /**
+     * Creates a new ButtonBuilder instance with the given boolean value button.
+     * <p>
+     * ButtonBuilder is a builder class that allows for chaining of conditions.
+     *
+     * @param button the button to listen for
+     * @return a new ButtonBuilder instance
+     */
     public static ButtonBuilder whileTrue(boolean button) {
         return new ButtonBuilder().whileTrue(button);
     }
 
+    /**
+     * Creates a new ButtonBuilder instance with the given boolean value button.
+     * <p>
+     * ButtonBuilder is a builder class that allows for chaining of conditions.
+     *
+     * @param button the button to listen for
+     * @return a new ButtonBuilder instance
+     */
     public static ButtonBuilder toggleOnTrue(boolean button) {
         return new ButtonBuilder().toggleOnTrue(button);
     }
@@ -37,14 +59,26 @@ public class ButtonListener {
             return this;
         }
 
-        public ButtonBuilder and(boolean button) {
-            ButtonListenerInstance.buttonList.add(button);
+        /**
+         * Adds a condition to the button listener.
+         *
+         * @param condition the condition to add
+         * @return the ButtonBuilder instance
+         */
+        public ButtonBuilder and(boolean condition) {
+            ButtonListenerInstance.buttonList.add(condition);
             ButtonListenerInstance.button = ButtonListenerInstance.buttonList.stream().allMatch(Boolean::booleanValue);
             return this;
         }
 
-        public ButtonBuilder or(boolean button) {
-            ButtonListenerInstance.buttonList.add(button);
+        /**
+         * Adds a condition to the button listener.
+         *
+         * @param condition the condition to add
+         * @return the ButtonBuilder instance
+         */
+        public ButtonBuilder or(boolean condition) {
+            ButtonListenerInstance.buttonList.add(condition);
             ButtonListenerInstance.button = ButtonListenerInstance.buttonList.stream().anyMatch(Boolean::booleanValue);
             return this;
         }
@@ -57,10 +91,21 @@ public class ButtonListener {
             return this;
         }
 
+        /**
+         * Runs the given Runnable.
+         *
+         * @param runnable the Runnable to run
+         */
         public void run(Runnable runnable) {
             this.run(runnable, Optional.empty());
         }
 
+        /**
+         * Runs the given Runnable if the conditions are true, otherwise runs the elseRunnable.
+         *
+         * @param runnable the Runnable to run if the conditions are true
+         * @param elseRunnableOptional the Runnable to run if the conditions are false
+         */
         public void run(Runnable runnable, Optional<Runnable> elseRunnableOptional) {
             if (ButtonListenerInstance.button) {
                 runnable.run();
